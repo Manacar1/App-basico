c08369d58783bb3dda092fe93e67599127a0bcdc
Manacar1 <lucca.porto.design@gmail.com>
2023-08-02 23:32:23 -0300

    02/08/2023

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const db = require('./db/db');
+const db = require('C:\\Users\\INTEL\\Documents\\GitHub\\App-basico\\db');
 
 const PORT = process.env.PORT || 3000;
 
